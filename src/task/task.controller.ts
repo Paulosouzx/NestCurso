71f164060c7f7ba479a70edec1c7acc2db8b1e92
Paulo Souza <paulosouzx14@gmail.com>
2025-01-19 21:45:35 +0000

    feat: implements remove and findAll tasks

--- src/task/task.controller.ts
+++ src/task/task.controller.ts
@@ -1,14 +1,43 @@
 import { TaskService } from './task.service';
-import { Body, Controller, Post } from '@nestjs/common';
-import { TaskDto } from './task.dto.js';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Put,
+  Query,
+} from '@nestjs/common';
+import { FindAllParameters, TaskDto } from './task.dto.js';
 
 @Controller('task')
 export class TaskController {
   //vai verificar se ja esta instanciado, para economizar recursos de memoria
-  constructor(private readonly TaskService: TaskService) {}
+  constructor(private readonly taskService: TaskService) {}
 
   @Post()
   create(@Body() task: TaskDto) {
-    this.TaskService.create(task);
+    this.taskService.create(task);
+  }
+
+  @Get('/:id')
+  findById(@Param('id') id: string): TaskDto {
+    return this.taskService.findById(id);
+  }
+
+  @Put()
+  update(@Body() task: TaskDto) {
+    this.taskService.update(task);
+  }
+
+  @Delete('/:id')
+  remove(@Param('id') id: string) {
+    return this.taskService.remove(id);
+  }
+
+  @Get()
+  findAll(@Query() params: FindAllParameters): TaskDto[] {
+    return this.taskService.findAll(params);
   }
 }
--- src/task/task.service.ts
+++ src/task/task.service.ts
@@ -1,5 +1,11 @@
-import { Injectable } from '@nestjs/common';
-import { TaskDto } from './task.dto';
+import {
+  BadRequestException,
+  HttpException,
+  HttpStatus,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
+import { FindAllParameters, TaskDto } from './task.dto';
 
 @Injectable()
 export class TaskService {
@@ -8,5 +14,52 @@ export class TaskService {
   create(task: TaskDto) {
     this.tasks.push(task);
     console.log(this.tasks);
+  }
+
+  findById(id: string): TaskDto {
+    const foundTask = this.tasks.filter((t) => t.id === id);
+
+    //quantidade de elementos dentro de meu foundTask
+    if (foundTask.length) return foundTask[0];
+
+    throw new HttpException(
+      `Task with id ${id} not found.`,
+      HttpStatus.BAD_REQUEST,
+    );
+  }
+
+  findAll(params: FindAllParameters): TaskDto[] {
+    return this.tasks.filter((t) => {
+      let match = true;
+
+      //vai verificar se o title esta incluso nos parametros passados, caso seja diferente ele nao vai trazer e o mesmo para status
+      if (params.title != undefined && !t.title.includes(params.title)) match = false;
+
+      if (params.status != undefined && !t.status.includes(params.status))
+        match = false;
+
+      return match;
+    });
+  }
+
+  update(task: TaskDto) {
+    let taskIndex = this.tasks.findIndex((t) => t.id === task.id);
+
+    if (taskIndex >= 0) this.tasks[taskIndex] = task;
+
+    throw new HttpException(`Task with id ${task.id}`, HttpStatus.BAD_REQUEST);
+  }
+
+  remove(id: string) {
+    let taskIndex = this.tasks.findIndex((t) => t.id === id);
+
+    if (taskIndex >= length) {
+      this.tasks.splice(taskIndex, 1);
+      return;
+    }
+    throw new HttpException(
+      `Task with id ${id} not found`,
+      HttpStatus.BAD_REQUEST,
+    );
   }
 }
