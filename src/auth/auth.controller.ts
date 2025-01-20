00652e7f16cb50a777a6d1e6197a2c21cc29ae7f
saradelimalira <sarinhaportugal2@gmail.com>
2025-01-20 18:39:58 +0000

    feat: start to implement auth features

---
+++ src/auth/auth.controller.ts
@@ -0,0 +1,17 @@
+import { Body, Controller } from '@nestjs/common';
+import { AuthResponseDto } from './auth.dto';
+import { AuthService } from './auth.service';
+
+@Controller('auth')
+export class AuthController {
+
+    constructor(private readonly authService: AuthService) { }
+
+    @Post('login')
+    singIn(
+        @Body('username') username: string,
+        @Body('password') password: string
+    ): AuthResponseDto{
+        return this.authService.singIn(username, password)
+    }
+}
